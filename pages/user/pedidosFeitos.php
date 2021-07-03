e53cf6a8b05954bb1436c670cf2f5b44b4dbbc9e
Francisco <franciscodamana@gmail.com>
2021-07-03 17:59:23 +0200

    Pedidos feitos

---
+++ pages/user/pedidosFeitos.php
@@ -0,0 +1,51 @@
+<?php include('../commons/header.php');?>
+<?php include('../commons/userMenu.php');?>
+<div class="content-wrapper">
+    <?php include('../commons/menuInterno.php')?>
+    <div class="content">
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="card card-primary">
+                            <div class="card-header">Pedidos de visto feitos</div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="card card-info">
+                                            <div class="card-header">Nome do visto</div>
+                                            <div class="card-body">
+                                                <ul>
+                                                    <li>Validade do visto</li>
+                                                    <li>taxa do visto</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer">
+                                            <button class="btn btn-sm btn-success">Fazer pedido</button>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <div class="card card-info">
+                                            <div class="card-header">Nome do visto</div>
+                                            <div class="card-body">
+                                                <ul>
+                                                    <li>Validade do visto</li>
+                                                    <li>taxa do visto</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer">
+                                            <button class="btn btn-sm btn-success">Fazer pedido</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> 
+<?php include('../commons/footer.php') ?>
