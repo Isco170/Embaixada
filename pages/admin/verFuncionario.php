1be52591c6eed5c3463b81c345cca2bcbbe14295
Francisco <franciscodamana@gmail.com>
2021-07-03 16:44:56 +0200

    Listar funcionarios

--- pages/admin/verFuncionario.php
+++ pages/admin/verFuncionario.php
@@ -5,9 +5,58 @@
     <div class="content">
         <div class="container-fluid">
             <div class="container">
-                
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <table id="datatableid" class="table table-bordered">
+                                    <thead>
+                                        <tr class="text-center">
+                                            <th>#</th> 
+                                            <th>Nome</th>
+                                            <th>Apelido</th>
+                                            <th>Con</th>
+                                        </tr>
+                                    </thead> 
+                                    <tbody>
+                                        <tr class="text-center">
+                                            <td>1</td>
+                                            <td>Nome </td>
+                                            <td>Apelido</td>
+                                            <td>Contact</td>
+                                        </tr>
+                                        <tr class="text-center">
+                                            <td>2</td>
+                                            <td>Nome </td>
+                                            <td>Apelido</td>
+                                            <td>Contact</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 <?php include('../commons/footer.php') ?>
+
+<script>
+  $(document).ready(function(){
+        $('#datatableid').DataTable({
+            "pagingType" : "full_numbers",
+            "lengthMenu" : [
+                [10, 25, 50, -1],
+                [10, 25, 50, "All"]
+            ],
+            responsive: true,
+            language: {
+                search : "_INPUT_",
+                searchPlaceholder: "Procurar funcionário",
+            }
+        });
+    });
+
+</script>
--- pages/commons/menu.php
+++ pages/commons/menu.php
@@ -15,13 +15,13 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item has-treeview"> 
-            <a href="../../pages/admin/verFuncionario.php" class="nav-link">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-graduation-cap"></i> 
                 <p>Funcionários<i class="right fas fa-angle-left"></i></p>
             </a> 
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="../../pages/admin/verFuncionario.php" class="nav-link">
                   <i class="fa fa-list"></i>
                   <p>Ver funcionários</p> 
                 </a>
