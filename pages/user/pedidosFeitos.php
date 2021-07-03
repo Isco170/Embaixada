da2da51d28ea74cc865c31eb17f7dfbef1543cec
Francisco <franciscodamana@gmail.com>
2021-07-03 18:09:01 +0200

    Trabalhando ver pedidos

--- pages/user/pedidosFeitos.php
+++ pages/user/pedidosFeitos.php
@@ -12,32 +12,30 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col">
-                                        <div class="card card-info">
-                                            <div class="card-header">Nome do visto</div>
-                                            <div class="card-body">
-                                                <ul>
-                                                    <li>Validade do visto</li>
-                                                    <li>taxa do visto</li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer">
-                                            <button class="btn btn-sm btn-success">Fazer pedido</button>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="card card-info">
-                                            <div class="card-header">Nome do visto</div>
-                                            <div class="card-body">
-                                                <ul>
-                                                    <li>Validade do visto</li>
-                                                    <li>taxa do visto</li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer">
-                                            <button class="btn btn-sm btn-success">Fazer pedido</button>
-                                        </div>
+                                        <table id="datatableid" class="table table-bordered">
+                                            <thead>
+                                                <tr class="text-center">
+                                                    <th>#</th>
+                                                    <th>Codigo</th> 
+                                                    <th>Nome</th>
+                                                    <th>Apelido</th>
+                                                    <th>Classe</th>
+                                                    <th class="text-center">Opções</>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td> <?php echo $c+1; ?> </td>
+                                                    <td> <?php echo $alunos[$c]->codigo; ?> </td>
+                                                    <td> <?php echo $alunos[$c]->aluno_nome; ?> </td>
+                                                    <td> <?php echo $alunos[$c]->aluno_apelido; ?> </td>
+                                                    <td> <?php echo $alunos[$c]->classe_designacao; ?> </td>
+                                                    <td class="project-actions text-center">
+                                                        <button class="btn btn-primary btn-sm" onclick="apagar(1)"><i class="fas fa-folder"></i>Cancelar pedido</button>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
                                     </div>
                                 </div>
                             </div>
@@ -49,3 +47,82 @@
     </div>
 </div> 
 <?php include('../commons/footer.php') ?>
+
+<script>
+    function apagar(id){
+    swal({
+
+        title: "Tem certeza?",
+
+        text: "Se apagar este aluno não poderá recuperar seus dados",
+
+        type: "warning",
+
+        buttons: true,
+
+        dangerMode: true,
+
+        showCancelButton: true,
+
+        confirmButtonColor: "red",
+
+        confrmButtonText: "Sim, apagar",
+
+        closeOnConfirm: false,
+
+    },
+    function(){
+
+        $.ajax({
+
+            url: "#",
+
+            type: "post",
+
+            data: {
+
+                id : id
+
+            },
+
+            success: function(){
+
+                swal({
+
+                    title : "Sucesso",
+
+                    text: "Aluno apagado",
+
+                    type: "success",
+
+                });
+
+                window.location.href = window.location.href;
+
+                
+
+            },
+
+            error: function(){
+
+                swal({
+
+                    title : "Falha",
+
+                    text : "Nao foi possivel apagar aluno",
+
+                    type : "error",
+
+                });
+
+            }
+
+        });
+
+
+
+    });
+
+    }
+
+</script>
