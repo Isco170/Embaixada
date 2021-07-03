1265c64ff97124c1116ed709f5eeb155a4399ccc
Francisco <franciscodamana@gmail.com>
2021-07-03 17:32:24 +0200

    Novo pedido

---
+++ pages/commons/userMenu.php
@@ -0,0 +1,44 @@
+  <!-- Main Sidebar Container -->
+
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <a href="#" class="brand-link">
+      <img src="../../../Embaixada/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+        <span class="brand-text font-weight-light">Embaixada</span>
+
+    </a>
+
+    <!-- Sidebar -->
+
+    <div class="sidebar">
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item has-treeview"> 
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-graduation-cap"></i> 
+                <p>Pedidos<i class="right fas fa-angle-left"></i></p>
+            </a> 
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../../pages/admin/verFuncionario.php" class="nav-link">
+                  <i class="fa fa-list"></i>
+                  <p>Ver pedidos</p> 
+                </a>
+                <a href="../../pages/admin/novoFuncionario.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                    <p>Novo pedido</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+
+      <!-- /.sidebar-menu -->
+
+    </div>
+
+    <!-- /.sidebar -->
+
+  </aside>
